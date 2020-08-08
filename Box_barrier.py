426879e35c6327058a3f271d93b1e8bcea37a18c
SumuduKMadushanka <ngsmadushanka.16@cse.mrt.ac.lk>
2020-08-08 16:49:12 +0530

    Bug fix

--- Box_barrier.py
+++ Box_barrier.py
@@ -1,12 +1,18 @@
 ## Created by : Sumudu Madushanka
-## Last update : 8/5/2020
+## Last update : 8/8/2020
 
 # Create the box barrier
 def create_box(dis, dis_width, dis_height, snake_block, font_size, barrier_grid):
     for i in range (dis_width//snake_block):
         barrier_grid.append([snake_block * i, font_size + 10])
+        barrier_grid.append([snake_block * i, font_size + 10 + snake_block])
+
         barrier_grid.append([snake_block * i, dis_height - snake_block])
+        barrier_grid.append([snake_block * i, dis_height - (2 * snake_block)])
 
     for i in range ((dis_height - (font_size + 10))//snake_block):
         barrier_grid.append([0, ((snake_block * i) + font_size + 10)])
+        barrier_grid.append([snake_block, ((snake_block * i) + font_size + 10)])
+
         barrier_grid.append([dis_width - snake_block, ((snake_block * i) + font_size + 10)])
+        barrier_grid.append([dis_width - (2 * snake_block), ((snake_block * i) + font_size + 10)])
--- Rail.py
+++ Rail.py
@@ -1,5 +1,5 @@
 ## Created by : Sumudu Madushanka
-## Last update : 8/5/2020
+## Last update : 8/8/2020
 
 # Create the Rail
 def create_rail(dis, dis_width, dis_height, snake_block, font_size, barrier_grid):
@@ -8,16 +8,28 @@ def create_rail(dis, dis_width, dis_height, snake_block, font_size, barrier_grid
     
     for i in range (dis_width//snake_block):
         barrier_grid.append([snake_block * i, font_size + 10])
+        barrier_grid.append([snake_block * i, font_size + 10 + snake_block])
+
         barrier_grid.append([snake_block * i, dis_height - snake_block])
+        barrier_grid.append([snake_block * i, dis_height - (2 * snake_block)])
 
     for i in range (tun_width//snake_block, (3*tun_width)//snake_block):
         barrier_grid.append([(snake_block * i), ((tun_height//snake_block) * snake_block) + font_size + 10])
+        barrier_grid.append([(snake_block * i), ((tun_height//snake_block) * snake_block) + font_size + 10 + snake_block])
+
         barrier_grid.append([(snake_block * i), ((3 * (tun_height//snake_block)) * snake_block) + font_size + 10])    
+        barrier_grid.append([(snake_block * i), ((3 * (tun_height//snake_block)) * snake_block) + font_size + 10 - snake_block])    
 
     for i in range (tun_height//snake_block):
-        barrier_grid.append([0, ((snake_block * i) + font_size + 10)])        
+        barrier_grid.append([0, ((snake_block * i) + font_size + 10)])
+        barrier_grid.append([snake_block, ((snake_block * i) + font_size + 10)])
+
         barrier_grid.append([dis_width - snake_block, ((snake_block * i) + font_size + 10)])
+        barrier_grid.append([dis_width - (2 * snake_block), ((snake_block * i) + font_size + 10)])
         
     for i in range (3*tun_height//snake_block, (dis_height - (font_size + 10))//snake_block):
         barrier_grid.append([0, ((snake_block * i) + font_size + 10)])
+        barrier_grid.append([snake_block, ((snake_block * i) + font_size + 10)])
+
         barrier_grid.append([dis_width - snake_block, ((snake_block * i) + font_size + 10)])
+        barrier_grid.append([dis_width - (2 * snake_block), ((snake_block * i) + font_size + 10)])
