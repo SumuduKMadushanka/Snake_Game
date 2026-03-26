59b0bc69a5ee7dc939eefecfe2c65590f1e8aa43
Sumudu Madushanka <ngsmadushanka@gmail.com>
2026-03-26 20:41:14 +0530

    Init Game colours enum

---
+++ enums/colours.py
@@ -0,0 +1,14 @@
+from enum import Enum
+
+class Colour(Enum):
+    WHITE = [255, 255, 255]
+    BLACK = [0, 0, 0]
+    YELLOW = [255, 255, 0]
+    LIGHTYELLOW = [255, 255, 224]
+    RED = [255, 0, 0]
+    GREEN = [0, 255, 0]
+    LIGHTGREEN = [144, 238, 144]
+    DARKGREEN = [0, 100, 0]
+    BLUE = [0, 0, 255]
+    LIGHTBLUE = [173, 216, 230]
+    DARKBLUE = [0, 0, 139]
